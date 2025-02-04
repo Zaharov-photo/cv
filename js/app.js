3f61391b617ba93ec7e15cef0bcf4e0de315c573
Zaharov-photo <Zaharov.photo@yandex.ru>
2025-02-04 11:04:05 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,8 @@
 window.addEventListener('scroll', e => {
-    document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`);
-});
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-// Инициализация ScrollSmoother
-const smoother = ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content',
-    smooth: 1.5, // Уровень плавности скролла
-    effects: true, // Включаем эффекты
-    normalizeScroll: true, // Нормализация скролла
-    ignoreMobileResize: true, // Игнорировать изменение размера на мобильных устройствах
-});
-
-// Разрешаем pointer-events для иконок
-smoother.effects('.hero_link', { speed: 1, lag: 0.1 });
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
